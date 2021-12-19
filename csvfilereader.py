0d81eb060a43445257e0e0cc6df49d0da6988ccf
Lovászi Márton <lovaszimarci@gmail.com>
2021-12-19 23:36:49 +0100

    'final'

--- csvfilereader.py
+++ csvfilereader.py
@@ -1,23 +1,28 @@
 import csv
 
-
-
-
 with open('1.csv', 'r') as file1:
     lista1 = csv.reader(file1)
+    for szam in lista1:
+        li1  =  szam
     #ll = list(lista1)
 
 with open('2.csv', 'r') as file2:
     lista2 = csv.reader(file2) 
-    masodiklista = list(lista2)
+    for szam in lista2:
+        li2 = szam
     
-elsolista = []
+osszeslista = li1 + li2
+osszeslista = list(osszeslista)
 
-elsolista.append(lista1)
+for char in osszeslista:
+    if osszeslista.count(char) > 1:
+        osszeslista.remove(char)
+        osszeslista.remove(char)
+        
+osszeslista = str(osszeslista)
 
-print(elsolista)
+
+with open('3.csv', 'w') as file3:
+    file3.write(osszeslista)
 
 
-# osszeslista = elsolista + masodiklista
-
-# print(osszeslista)
